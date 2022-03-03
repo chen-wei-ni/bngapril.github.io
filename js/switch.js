e56961ebfc8f53299fdbf7af1afcc57f708fcc1d
chen-wei-ni <bcg2135@gmail.com>
2022-03-03 09:13:55 +0800

    renew

---
+++ js/switch.js
@@ -0,0 +1,38 @@
+$('.switch-list').hide()
+$('.language-pack').click(function(e){
+    e.stopPropagation();
+    $('.switch-list').toggle(300, 'linear')
+})
+$("body").click(function(){
+    $('.switch-list').hide()
+})
+$('.switch-list').click(function(e){
+    e.stopPropagation();
+})
+
+$('.top_btn').hide();
+		$(window).scroll(function() {
+			var sh = $(window).scrollTop();
+			
+			if(sh > 1000) {
+				$('.top_btn').show(500);
+			}else {
+				$('.top_btn').hide(500, 'swing');
+			}
+		});
+		
+		$('.top_btn').click(function() {
+			$('html,body').animate({
+				scrollTop : 0 //到最上面  
+			}, 500);
+		return false;
+		});
+
+/*const animate = document.querySelector('.effect');
+animate.classList.add('heartbeat');
+animate.addEventListener('animationend', () => {
+animate.classList.remove('heartbeat');  
+setTimeout(function(){
+animate.classList.add('heartbeat');
+},2000)
+})*/
